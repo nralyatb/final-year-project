719fbe78260591d86f7eda97f0466a9813738043
nralyatb <114821094+nralyatb@users.noreply.github.com>
2022-12-23 02:00:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,22 @@ from img_classification import teachable_machine_classification
 st.title("Traditional Kuih Classification")
 st.subheader("This app is built to classify traditional kuih. Upload a traditional kuih image for image classification to identify what kuih it is")
 
-
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('Streamlit FYP Background.png')  
 
 
 uploaded_file = st.file_uploader("Upload a traditional kuih image...", type="jpg")
